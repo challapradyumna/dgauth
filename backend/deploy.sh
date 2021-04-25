5b5bacc06d88b32305f4db377da7fc229a7fac93
Challa Pradyumna <challapradyumna@gmail.com>
2021-04-25 16:03:08 +0530

    FIX: deploy script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-# docker build . -t localhost:32000/dgauth
-# docker push localhost:32000/dgauth
+!/bin/bash
+docker build . -t localhost:32000/dgauth
+docker push localhost:32000/dgauth
 docker build . -f Dockerfile.playground -t localhost:32000/dgauth:playground
 docker push localhost:32000/dgauth:playground
 microk8s kubectl apply -f deploy.yaml
